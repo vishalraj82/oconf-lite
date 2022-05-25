fb2367aa4e7ca1b408d797159ada7c242fb18ee8
Gustav Nikolaj Olsen <gustavnikolaj@gmail.com>
2022-05-26 00:19:22 +0200

    Initial commit

---
+++ test/oconf-lite.test.js
@@ -0,0 +1,8 @@
+import expect from "unexpected";
+import oconfLite from "../lib/oconf-lite.js";
+
+describe("oconf-lite", () => {
+  it("should be a function", () => {
+    expect(oconfLite, "to be a function");
+  });
+});
