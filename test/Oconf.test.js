23bebd154491d24f0de9a51ec95c34ff9b10e720
Gustav Nikolaj Olsen <gustavnikolaj@gmail.com>
2022-05-26 00:41:16 +0200

    Add Oconf class

---
+++ test/Oconf.test.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+
+import Oconf from "../lib/Oconf.js";
+import expect from "unexpected";
+
+describe("Oconf", () => {
+  it("should throw when instantiated without a parser", () => {
+    expect(() => new Oconf(), "to throw", /parser/);
+  });
+});
